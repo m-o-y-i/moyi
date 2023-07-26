452f25f11293bc8b1397492a425e4c9a13815554
Yousi1 <yousi1@ip-192-168-1-103.ap-northeast-1.compute.internal>
2023-07-26 13:09:16 +0800

    update articele

--- back/src/routes/v2/routes/article.ts
+++ back/src/routes/v2/routes/article.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
 import client from '../../../mongoDB'
+import { nanoid } from 'nanoid';
 
 const router = Router();
 
@@ -25,6 +26,23 @@ router.post('/article_detail', async (req: Request, res: Response) => {
         const article = client.db("article").collection("article")
         const data = await article.findOne({ _id: id })
         res.status(200).json({ code: 200, status: true, data, message: 'success' })
+    } catch (error) {
+        res.status(200).json({ code: 200, status: false, message: error })
+    }
+    finally {
+        // 关闭数据库连接
+        await client.close();
+    }
+});
+
+router.post('/write_article_detail', async (req: Request, res: Response) => {
+    await client.connect()
+    try {
+        const { title, describe, content } = req.body
+        const article = client.db("article").collection("article")
+        const _id = nanoid();
+        await article.insertOne({ _id, title, describe, content })
+        res.status(200).json({ code: 200, status: true, message: 'success' })
     } catch (error) {
         res.status(200).json({ code: 200, status: false, message: error })
     }
